f8a1f5cce900c02a40082f7e091d6667d291d29b
fabiandn <fabian.arturo.diaz.espinosa@outlook.com>
2016-07-25 14:47:53 -0500

    add projects sample

---
+++ 70483/chapter1/threads/sample1/sample1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace sample1
+{
+    class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc:{0}", i);
+                Thread.Sleep(0);
+            }
+        }
+
+        public static void Main()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Mainthread: Dosomework.");
+                Thread.Sleep(0);
+            }
+            t.Join();
+        }
+    }
+}
